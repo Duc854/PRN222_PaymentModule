09884d735e4ea4c440557d298095d0eae37ca36a
Duc854 <nmduc854@gmail.com>
2025-10-01 16:20:38 +0700

    Initial commit - push MVC 3 layer project

---
+++ PaymentModule.Web/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentModule.Data.Entities;
+
+namespace PaymentModule.Web.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add SqlServer
+            var connectionString = configuration.GetConnectionString("SQLServerConnection");
+            services.AddDbContext<CloneEbayDbContext>(options => options.UseSqlServer(connectionString));
+
+            //Add Services
+
+            //Add Repositories
+
+            return services;
+        }
+    }
+}
